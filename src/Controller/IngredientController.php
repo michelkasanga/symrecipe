12c337434c4e381440900eaff18f0cd12ac704b5
michelKasanga <kasangamichel1@gmail.com>
2022-10-22 20:23:48 +0200

    CRUD ingredient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -43,11 +43,32 @@ public function new (Request $request, IngredientRepository $repository): Respon
 }
 
 #[Route('/ingredient/edit/{id}', name:'app_ingredient_edit', methods:['GET', 'POST'])]
-public function edit(IngredientRepository $repository, $id): Response
+public function edit( Request $request, IngredientRepository $repository, Ingredient $ingredient): Response
 {
-    $ingredient = $repository->findOneBy([ "id" => $id ]);
     $form = $this->createForm(IngredientType::class, $ingredient);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $repository->save($form->getData(), true);
+        $this->addFlash(
+            'success',
+            'votre ingredient a ete modifier avec success!'
+        );
+        return $this->redirectToRoute('app_ingredient');
+    }
     return $this->render('pages/ingredient/edit.html.twig',  ['form' => $form->createView()]);
+}
+
+
+#[Route('/ingredient/delete/{id}', name:'app_ingredient_delete', methods:[ 'GET'])]
+public function delete(Ingredient $ingredient, IngredientRepository $repository): Response
+{
+    $repository->remove($ingredient, true);
+        $this->addFlash(
+            'success',
+            'votre ingredient a ete supprimer avec success!'
+        );
+        return $this->redirectToRoute('app_ingredient');
 }
 
 #[Route('/model', name:'app_model')]
